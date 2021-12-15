cc7a4e5d21451473fcaf4385f47b3a3be04788f4
mmolho <michael.molho@e-xpertsolutions.com>
2021-12-15 16:22:12 +0100

    log4find/windows: version 2.0

--- log4shell/log4find/windows/README.md
+++ log4shell/log4find/windows/README.md
@@ -2,68 +2,75 @@
 
 ## Requirements
 
- - Run on potentially any Windows based system using Powershell
+ - Run on potentially any Windows based system with Powershell available
  - Tested successfully on Windows 2012 Server and Windows 2016
  - Administrator rights
- - Presence of "handle.exe" from Microsoft Sysinternals ([Handle - Windows Sysinternals | Microsoft Docs])(https://docs.microsoft.com/en-us/sysinternals/downloads/handle)
- - One READ_ONLY network share to host the "handle.exe" binary, with "Domain Users" and "Domain Computers" included (both on network share and file system security properties). **Pay attention to the fact that allowing write in this share may represent a high security risk as it may allow any attacker to modify this file and run arbitrary command as SYSTEM on all your environment.**
+ - Download of "handle.exe" from Microsoft Sysinternals ([Handle - Windows Sysinternals | Microsoft Docs])(https://docs.microsoft.com/en-us/sysinternals/downloads/handle)
+  
 
-## Usage
+## Simple usage: local execution
 
-Before use, please read the "***Configuration***" part bellow
+**Before anything**: the file "handle.exe" from Microsoft Sysinternals must be downloaded [here](https://docs.microsoft.com/en-us/sysinternals/downloads/handle)
 
-Local usage from a CMD shell as Administrator (default):
+Local usage from a CMD shell as Administrator (default)
 
-`powershell -ExecutionPolicy Bypass -File log4find.ps1`
-    
-Deployed on a whole domain usage : Follow the "***Configuration***" part and the section "**For large scale deployment (on a whole domain for example)**"
+`powershell -ExecutionPolicy Bypass -File log4find.ps1 -HandlePath <handle.exe_filepath>`
 
-## Configuration
+![](screenshot-windows.png)
 
-You must configure different variables for the script to run properly.
+## Advanced usage: GPO deployment and central logging
 
-**In any case:**
- - The following variable is the UNC path to your "handle.exe" binary. This UNC path must be configured with the following requirements (READ_ONLY for "Domain Users" and "Domain Computers")
- 
-`$handle_exe_path = '\\READ_ONLY_SHARE\sysinternals\handle.exe -accepteula'`
+Another option is to schedule the deployment and execution of this script through all your Windows domain through GPO and collect all the outputs to a central logs directory. 
 
-**For large scale deployment (on a whole domain for example):**
-You must configure an additional network share to host outputs from your machines. In such a case, this network share must have READ and WRITE permissions for both "Domain Users" and "Domain Computers" at network share security level and file system level. In this situation you must set the following variables:
+To do that, the file "handle.exe" must be available for all the machines. 
 
-    $deployment_with_gpo = 1
-    $centralized_unc = "\\WRITABLE_SHARE\$srv-log4jensic.txt"
+You can host that file in a **READ ONLY** file share accessible to all machines : \\\READ_ONLY_SHARE\handle.exe. 
 
-Then you have to copy the log4find.ps1 
+You need to make sure this shared folder is **READ ONLY** and allowed for both groups "Domain Users" and "Domain Computers" (both on network share and file system security properties). 
+
+**Pay attention to the fact that this file share must be READ ONLY ! Allowing write access to this share by mistake represents a high security risk as it may allow any attacker to modify this file and run arbitrary command as SYSTEM on all your environment.**
+
+In addition, you need a another **WRITABLE** shared folder to receive all the outputs of the script executed by all the machines. 
+
+Once you have these two shared folders, you can test it works as expected on a single machine with this command :
+
+`powershell -ExecutionPolicy Bypass -File log4find.ps1 -HandlePath "\\READ_ONLY_SHARE\handle.exe" -LoggingDir "\\WRITABLE_SHARE\"`
+
+If working as expected, you should get a logging file ${hostname}.log in the folder \\\WRITABLE_SHARE
+
+Then you can configure the GPO to schedule immediate execution of the script to all machines through GPO
+
+### GPO Configuration
 
 The easiest deployment process can be performed using Scheduled Task (GPO). For this you can follow the procedure below (extracted from https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/configure-endpoints-gp?view=o365-worldwide and modified)
 
-1.  **Copy "log4find.ps1" in the READ_ONLY share** you created at first (not the READ/WRITE one) 
+1.  **Copy "log4find.ps1" in the READ ONLY share** you created at first : \\\READ_ONLY_SHARE\log4find.ps1
 2. To create a new GPO, open the  [Group Policy Management Console](https://docs.microsoft.com/en-us/internet-explorer/ie11-deploy-guide/group-policy-and-group-policy-mgmt-console-ie11)  (GPMC), right-click  **Group Policy Objects**  you want to configure and click  **New**. Enter the name of the new GPO in the dialogue box that is displayed and click  **OK**.
-    
+	
 3.  Open the  [Group Policy Management Console](https://docs.microsoft.com/en-us/internet-explorer/ie11-deploy-guide/group-policy-and-group-policy-mgmt-console-ie11)  (GPMC), right-click the Group Policy Object (GPO) you want to configure and click  **Edit**.
-    
+	
 4.  In the  **Group Policy Management Editor**, go to  **Computer configuration**, then  **Preferences**, and then  **Control panel settings**.
-    
+	
 5.  Right-click  **Scheduled tasks**, point to  **New**, and then click  **Immediate Task (At least Windows 7)**.
-    
+	
 6.  In the  **Task**  window that opens, go to the  **General**  tab. Under  **Security options**  click  **Change User or Group**  and type SYSTEM and then click  **Check Names**  then  **OK**. NT AUTHORITY\SYSTEM appears as the user account the task will run as.
-    
+	
 7.  Select  **Run whether user is logged on or not**  and check the  **Run with highest privileges**  check box.
-    
-8.  In the Name field, type an appropriate name for the scheduled task (for example, **Log4jfind**).
-    
+	
+8.  In the Name field, type an appropriate name for the scheduled task (for example, **Log4find 2.0**).
+	
 9.  Go to the  **Actions**  tab and select  **New...**  Ensure that  **Start a program**  is selected in the  **Action**  field. Enter the following in the  
 
  "Program/script:" 
 
-    powershell.exe 
+	powershell.exe 
 
  "Add arguments:" 
 
-    -ExecutionPolicy Bypass -File \\READ_ONLY_SHARE\log4find.ps1
+	-ExecutionPolicy Bypass -File \\READ_ONLY_SHARE\log4find.ps1 -HandlePath \\READ_ONLY_SHARE\handle.exe -LoggingDir \\WRITABLE_SHARE\
 
-    
+	
 11.  Select  **OK**  and close any open GPMC windows.
-    
+	
 12.  To link the GPO to an Organization Unit (OU), right-click and select  **Link an existing GPO**. In the dialogue box that is displayed, select the Group Policy Object that you wish to link. Click  **OK**.
 
--- log4shell/log4find/windows/log4find.ps1
+++ log4shell/log4find/windows/log4find.ps1
@@ -1,120 +1,157 @@
 ###########################################
-# Log4j - Windows forensic script
-# Must be run as Admin with Sysinternals tools installed 
+# Log4shell analysis script
+# Must be run as Admin after having downloaded Sysinternals Handle.exe tool 
 #
-# David Routin - 13.12.2021
+# David Routin
+# Michael Molho
 #
 
-$srv = hostname
-$proc = "*java*"
+# Parse parameters
+param($handlepath, $loggingdir)
+
+# Config
+$log4find_ver = "2.0"
 $pattern_log4j = "log4j"
 $pattern_forensic = "jndi:"
-$pattern_logfiles = ".log"
-$handle_exe_path = '\\READ_ONLY_SHARE\sysinternals\handle.exe'
-# 0 = False, 1 = True
-$deployment_with_gpo = 0
-$centralized_unc = "\\WRITABLE_SHARE\$srv-log4jensic.txt"
+$pattern_logfiles = "\.log|\.txt"
+$log4j_vuln_vers = "^(2\.[0-9]$)|(2\.1[01234]$)"
 
 # Test handle.exe is accessible
-if ((Test-Path $handle_exe_path) -eq $false) {
-    write-host "Handle.exe not accessible... Please download from sysinternals, leaving"
+if ( !$PSBoundParameters.ContainsKey('handlepath') ) {
+	Write-Host "ERROR: Parameter -HandlePath is required"
+	Write-Host "Download handle.exe from Sysinternal first (https://docs.microsoft.com/en-us/sysinternals/downloads/handle) and give this file path as parameter -HandlePath"
+	Write-Host "Examples:"
+	Write-Host "    .\log4find.ps1 -HandlePath C:\handle.exe"
+	Write-Host "    .\log4find.ps1 -HandlePath \\fileserver\RO_share\handle.exe -LoggingDir \\fileserver\RW_share\"
+	exit
+}
+if ( !(Test-Path $handlepath) ) {
+    Write-Host "ERROR: File '$handlepath' cannot be read, please check the path is correct"
     exit
 }
 
-
-#Exit if deployment with gpo is selected and file already exists
-if (($deployment_with_gpo -eq 1) -And ( Test-Path $centralized_unc)) {
-    exit
+# Test loggingdir exists
+if ( ($PSBoundParameters.ContainsKey('loggingdir')) -And !(Test-Path $loggingdir) ) {
+	Write-Host "ERROR: Log directory '$loggingdir' does not exist"
+	exit
 }
 
-if ($deployment_with_gpo -eq 1) {
-    echo "Starting..." | Out-File -FilePath $centralized_unc
+# Build logging file path from hostname + loggingdir
+$loggingpath = ""
+if ( $PSBoundParameters.ContainsKey('loggingdir') ) {
+    $hostname = hostname
+    $loggingpath = "${loggingdir}\\${hostname}.log"
 }
 
-$Log4js = @()
-$LogFiles = @()
-$exploited = @()
+#Exit if centralized logging is enabled and log file already exists
+if ( ($PSBoundParameters.ContainsKey('loggingdir')) -And (Test-Path $loggingpath) ) {
+    Write-Host "The script has run already, exiting ..."
+	exit
+}
 
-# Check process name based on java pattern
-$result_java_ids = Get-Process -Name $proc
+# Function for centralized logging
+function Log-Central {
+    param (
+        [string]$msg,
+		[string]$color,
+		[string]$loggingpath
+    )
+	# Display console
+	if ( $color ) {
+		Write-Host -Foregroundcolor $color $msg
+	} else {
+		Write-Host $msg
+	}
+	
+	# Central logging if enabled
+	if ( $loggingpath ) {
+		$timestamp = Get-Date -Format "MM/dd/yyyy HH:mm"
+		echo "[${timestamp}] $msg"  | Out-File -FilePath $loggingpath -Append
+	}
+}
 
-If ($result_java_ids -ne $null) {
+echo "                                                 ";
+echo "    __                __ __  _______           __";
+echo "   / /   ____  ____ _/ // / / ____(_)___  ____/ /";
+echo "  / /   / __ \/ __ \`/ // /_/ /_  / / __ \/ __  / ";
+echo " / /___/ /_/ / /_/ /__  __/ __/ / / / / / /_/ /  ";
+echo "/_____/\____/\__, /  /_/ /_/   /_/_/ /_/\__,_/   ";
+echo "            /____/                               ";
+echo "                                                 ";
+echo " ==> By e-Xpert Solutions  (David Routin/Michael Molho)";
+echo " ==> Version: $log4find_ver                      ";
+echo "                                                 ";
 
-    Write-Host $result_java_ids.Length process JAVA found
+# Check process name based on java patterns
+$result_java_ids = Get-Process "*java*", "*tomcat*", "*apache*"
+
+If ($result_java_ids) {
+
+    $nb_proc = $result_java_ids.Length
+    Log-Central -LoggingPath $loggingpath -Msg "$nb_proc process JAVA found`n"
 
     $result_java_ids | Foreach-Object { 
 
-		$id_process = $_.Id
-		$query = "SELECT CommandLine FROM Win32_Process WHERE ProcessID = "+ $id_process
-		$CommandLines = (Get-WmiObject -Query $query).CommandLine -join "`r`n"
+        # Get process commandline from WMI
+		$pid_process = $_.Id
+		$query = "SELECT CommandLine FROM Win32_Process WHERE ProcessID = "+ $pid_process
+		$CommandLine = (Get-WmiObject -Query $query).CommandLine 
 
-		### Getting handles for each PID and looking for log4j
+		### Get files handles 
+        $command_handles = $handlepath + " -accepteula -p " + $pid_process
+        $handles_output = cmd /c $command_handles | Select-String -Pattern 'File .+?:'
 
-        $command_handle = $handle_exe_path+" -accepteula -p " + $id_process
-        $cmd_output = cmd /c $command_handle | Select-String -Pattern 'File .+?:'
-
-        if ($cmd_output -match $pattern_log4j ) {
-            Write-Host -Foregroundcolor red "`n[WARNING][LOG4J] Lib usage detected "
+        ### Detect log4 in handles or in commandline
+        if ( ($handles_output -match $pattern_log4j) -or ($CommandLine -match $pattern_log4j) ) {
+            Log-Central -LoggingPath $loggingpath -Color yellow -Msg "[PID:$pid_process] *WARNING* log4j lib usage detected"
         } else {
-            Write-Host -Foregroundcolor green "`n[INFO][JAVA] detected without log4j (unsure) "
-            
+            Log-Central -LoggingPath $loggingpath -Color green -Msg "[PID:$pid_process] log4j does not seem to be used here"
         }
         
-        Write-Host `t`tPID: $id_process - $CommandLines 
+		# Display process commandline
+        Log-Central -LoggingPath $loggingpath -Msg "  [*] Process details:"
+		Log-Central -LoggingPath $loggingpath -Msg "    $CommandLine"
 
-        foreach ($line in $cmd_output) {
+        # Loop through all file handles looking for log4j
+		Log-Central -LoggingPath $loggingpath -Msg "  [*] log4j jar files in use:"
+        foreach ($line in $handles_output) {
             if ($line -match $pattern_log4j ) {
-                    $log4j_temp = [regex]::match($line, "File.+?\s+(\S+)?\s+(?<filename>.+?)$").Groups["filename"].Value
-                    
-                    Write-Host -Foregroundcolor red `t`t[LOG4J_PATH] $log4j_temp
-
-                    $Log4js += $log4j_temp
+                    $log4j_path = [regex]::match($line, "File.+?\s+(\S+)?\s+(?<filename>.+?)$").Groups["filename"].Value
+					# Try to detect lib4j version from filename
+					$log4j_fname = Split-Path $log4j_path -leaf
+					$log4j_vers_match = [regex]::match($log4j_fname, "\-([0-9]+\.[0-9]+)")
+					if ( $log4j_vers_match.Success ) {
+						$log4j_vers = $log4j_vers_match.Groups[1].Value
+						# Detect vulnerable version
+						if ( $log4j_vers -match $log4j_vuln_vers ) {
+						    Log-Central -LoggingPath $loggingpath -Color red -Msg "    [${log4j_vers}:VULN] $log4j_path"
+					    } else {
+							Log-Central -LoggingPath $loggingpath -Color green -Msg "    [${log4j_vers}:OK] $log4j_path"
+						}
+					} else {
+                        Log-Central -LoggingPath $loggingpath -Color yellow -Msg "    [???:TOCHECK] $log4j_path"
+					}
             }
-            if ($line -match $pattern_logfiles ) {
-                    $LogFile = [regex]::match($line, "File.+?\s+(\S+)?\s+(?<filename>.+?)$").Groups["filename"].Value
-                    $LogFiles += $LogFile
-                    $exploited_res = Get-Childitem -Path $LogFile| Select-String -pattern $pattern_forensic
-                    $exploited += $exploited_res
-                    Write-Host -Foregroundcolor green `t`t[OK] $LogFile
-            }
-
         } 
+		
+		# Loop through all file handles looking for log files / exploitation traces
+		Log-Central -LoggingPath $loggingpath -Msg "  [*] Looking for potential previous exploitation in open files:"
+		foreach ($line in $handles_output) {
+            if ($line -match $pattern_logfiles ) {
+				    $LogFile = [regex]::match($line, "File.+?\s+(\S+)?\s+(?<filename>.+?)$").Groups["filename"].Value
+                    $exploited = Get-Childitem -Path $LogFile | Select-String -pattern $pattern_forensic
+					if ( $exploited ) {
+                        Log-Central -LoggingPath $loggingpath -Color red -Msg "    [EXPLOIT] $LogFile"
+				    } else {
+						Log-Central -LoggingPath $loggingpath -Color green -Msg "    [OK] $LogFile"
+					}
+            }
+        }
 
-    }
-
-    
-    if ( $deployment_with_gpo -eq 1 ) {
-       if ($Log4js -ne $null) {
-                    $Log4js = $Log4js | sort -unique
-                    echo "[WARNING][Log4j] DETECTED in:" | Out-File -FilePath $centralized_unc -Append
-                    $Log4js = $Log4js -join "`r`n"
-                    echo $Log4js | Out-File -FilePath $centralized_unc -Append
-       }
-   
-       if ($LogFiles -ne $null) {
-                    $LogFiles = $LogFiles | sort -unique
-                    echo "`n[INFO][Logfiles] Identified LogFiles:" | Out-File -FilePath $centralized_unc -Append
-                    $LogFiles = $LogFiles -join "`r`n"
-                    echo $LogFiles | Out-File -FilePath $centralized_unc -Append
-       }
-
-       #### Looking for track of exploitations in LogFiles
-
-    }
-
-    if ($exploited -ne $null) {
-                $exploited = $exploited | sort -unique
-                Write-Host -Foregroundcolor red "`n[CRITICAL][EXPLOIT] DETECTED in:" 
-                $exploited = $exploited -join "`r`n"
-                Write-Host $exploited 
-                if ( $deployment_with_gpo -eq 1 ) {
-                    echo "`n[CRITICAL][EXPLOIT] DETECTED in:"  | Out-File -FilePath $centralized_unc -Append
-                    echo $exploited | Out-File -FilePath $centralized_unc -Append
-                }
     }
 
 } else {
-    Write-Host -Foregroundcolor green "[INFO][JAVA] No process found"
+    Log-Central -LoggingPath $loggingpath -Color green -Msg "No java' process found. This one is safe  ;-) `n"
 }
 
 
